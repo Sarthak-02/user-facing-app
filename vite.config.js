e3e2395a91ca69cf09f692ad75c98669f438df04
sarthak-smartq <sarthak.gupta@thesmartq.com>
2026-01-31 17:49:57 +0530

    injectionPoint: undefined,

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
         swSrc: path.resolve(__dirname, "src/sw.js"),
         swDest: "sw.js",
         globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
+        injectionPoint: undefined,
       },
       registerType: "autoUpdate",
       devOptions: {
